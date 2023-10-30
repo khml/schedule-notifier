436425df9374eea9281577f636448989a5740648
khml <43922054+khml@users.noreply.github.com>
2023-10-30 17:51:19 +0900

    [modify] improve package

--- schedulenotifier/lib.go
+++ schedulenotifier/lib.go
@@ -2,9 +2,24 @@ package schedulenotifier
 
 import (
 	"schedule-notifier/schedulenotifier/scheduletask"
+	"schedule-notifier/schedulenotifier/settings"
 	"schedule-notifier/schedulenotifier/ui"
 )
 
 func RunTUI(tasks []scheduletask.ScheduleTask) {
 	ui.BuildApp(tasks)
+}
+
+func ReadSchedule(filepath string) ([]scheduletask.ScheduleTask, error) {
+	taskDefs, err := settings.ReadSettingYaml(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	tasks, err := scheduletask.ReadDefine(taskDefs)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
 }
